464a012a2799ccfd5090fa8a103ba013e486231f
Andrii Mishchuk <a.mishchuk@steel.mil.ua>
2026-05-07 10:41:10 +0300

    minor code formatting changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //setup data
 const config = {
   columns: 10,
@@ -17,7 +19,6 @@ const counters = {
   wrong_flags_counter: 0,
   opened_tiles: 0
 };
-
 
 //calculate mines positions
 place_mines(all_tiles, config.mines_quantity);
@@ -42,7 +43,7 @@ for (let i = 0; i < config.tile_quantity; i++) {
   field.appendChild(tile);
 }
 
-//functions
+//ligic functions
 function place_mines(arr, mines_quantity) {
   let counter = mines_quantity;
   while (counter > 0) {
@@ -86,7 +87,9 @@ function array_of_tiles_around_tiles(index) {
       indeces = [tl, tt, tr, cl, cr, bl, bb, br];
   }
   //remove indeces outside top and bottom borders
-  return indeces.filter((element) => element >= 0 && element < config.tile_quantity,);
+  return indeces.filter(
+    (element) => element >= 0 && element < config.tile_quantity,
+  );
 }
 
 //ui functions
@@ -97,62 +100,67 @@ function open_cell() {
     mine.appendChild(text);
     this.classList.add("open");
     this.appendChild(mine);
-    
+
     counters.opened_tiles++;
 
-    
-    lose_game (all_tiles, this.id);
+    lose_game(all_tiles, this.id);
     win_game();
-    
+
     //move it to several different functions
-      if (all_tiles[this.id] == "" ) {
-        const arr_to_open = array_of_tiles_around_tiles(Number(this.id));
-        arr_to_open.map((element) => {
-          if (all_tiles[element] != config.mine_symbol)
-            open_cell.call(document.getElementById(`${element}`));
-        });
-      };
-  };
-};
+    if (all_tiles[this.id] == "") {
+      const arr_to_open = array_of_tiles_around_tiles(Number(this.id));
+      arr_to_open.map((element) => {
+        if (all_tiles[element] != config.mine_symbol)
+          open_cell.call(document.getElementById(`${element}`));
+      });
+    }
+  }
+}
 function place_flag() {
   if (!this.classList.contains("open")) {
     if (this.firstChild) {
       this.removeChild(this.firstChild);
-      
+
       counters.flag_counter--;
-      if (all_tiles[this.id] != config.mine_symbol) counters.wrong_flags_counter--;
-    
+
+      if (all_tiles[this.id] != config.mine_symbol)
+
+        counters.wrong_flags_counter--;
+        
     } else {
       const flag = document.createElement("p");
       const text = document.createTextNode(`${config.flag_symbol}`);
       flag.appendChild(text);
       this.appendChild(flag);
-      
+
       counters.flag_counter++;
-      if (all_tiles[this.id] != config.mine_symbol) counters.wrong_flags_counter++;
-      
+      if (all_tiles[this.id] != config.mine_symbol)
+        counters.wrong_flags_counter++;
+
       win_game();
     }
   }
 }
-function win_game(){
-  if ((counters.flag_counter == config.mines_quantity) &&
-      (counters.wrong_flags_counter == 0) &&
-      (counters.opened_tiles + counters.flag_counter == config.tile_quantity )){
-      end_game("You've won")
-    }
-}
-function end_game (message) {
-    const all_tiles = document.querySelectorAll(".tile");
-    all_tiles.forEach (tile =>{
-      tile.removeEventListener("click", open_cell);
-      tile.removeEventListener("contextmenu", place_flag);
-      tile.style["background-color"] = "green";
-    })
-    alert (`${message}`) //add reset function
+function win_game() {
+  if (
+    counters.flag_counter == config.mines_quantity &&
+    counters.wrong_flags_counter == 0 &&
+    counters.opened_tiles + counters.flag_counter == config.tile_quantity
+  ) {
+    end_game("You've won");
   }
-function lose_game (array, index) {
-  if (array[index] === config.mine_symbol){
-    end_game("You've lost")
+}
+function end_game(message) {
+  const all_tiles = document.querySelectorAll(".tile");
+  all_tiles.forEach((tile) => {
+    tile.removeEventListener("click", open_cell);
+    tile.removeEventListener("contextmenu", place_flag);
+    tile.style["background-color"] = "green";
+  });
+  alert(`${message}`); //add reset function
+}
+function lose_game(array, index) {
+  if (array[index] === config.mine_symbol) {
+    end_game("You've lost");
   }
 }
